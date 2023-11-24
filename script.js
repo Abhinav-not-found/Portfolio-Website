ba3eb07489e672f5aa720907700e4eb00fc8ad39
Abhinav-not-found <abhinavisgreat18@gmail.com>
2023-11-25 02:48:22 +0530

    updated navbar animation

--- script.js
+++ script.js
@@ -38,3 +38,14 @@ function github(){
 };
 
 
+var header = document.getElementById("btn-container");
+var btns=header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) { 
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+    });
+  }
